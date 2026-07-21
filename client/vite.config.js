99300dbc10bdc7e06877b792fcd375749d865420
Aerdelan <w1874863790@gmail.com>
2026-07-21 18:17:13 +0800

    fix: 修改数据库地址信息

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     port: 5173,
     proxy: {
       '/api': {
-        target: 'http://localhost:3001',
+        target: 'http://localhost:3002',
         changeOrigin: true,
         timeout: 1200000, // 20 分钟超时，用于番茄全本下载
         proxyTimeout: 1200000,
